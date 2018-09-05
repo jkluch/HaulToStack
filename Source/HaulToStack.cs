5de3fbe397288c839b0b3e5f0766a2272fd40937
Kluch <joe.kluchinski@gmail.com>
2018-09-04 22:58:04 -0400

    B.19 Update:
    I removed almost all the code I originally wrote.. it wasn't needed.  For some reason I decided I had to make this mod in the most convoluted way possible.. When updating for .19 I noticed I only needed to make a small change to get the behavior I wanted so I went ahead and removed all the unneeded buggy code.  Should work better now.

--- Source/HaulToStack.cs
+++ Source/HaulToStack.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Linq;
 using Harmony;
 using HugsLib;
 using HugsLib.Utils;
@@ -12,7 +12,7 @@ using Verse.AI;
 
 namespace HaulToStack
 {
-	public class HaulToStack : ModBase
+    public class HaulToStack : ModBase
     {
         public static HaulToStack Instance { get; private set; }
 
@@ -32,305 +32,62 @@ namespace HaulToStack
         }
 
     }
-    
+
     [HarmonyPatch]
-    static class Haul_Patch
+    static class PreToilReservations_Patch
     {
 
         static List<PlannedHauls> plannedHaulList = new List<PlannedHauls>();
 
         static MethodInfo TargetMethod()
         {
-            return AccessTools.Method(typeof(Verse.AI.HaulAIUtility), "HaulToStorageJob", new Type[] { typeof(Pawn), typeof(Thing) });
+            return AccessTools.Method(typeof(Verse.AI.JobDriver_HaulToCell), "TryMakePreToilReservations", new Type[] { typeof(bool) });
         }
-        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> original)
+
+        static bool Prefix()
         {
-            var originalMethod = AccessTools.Method(typeof(StoreUtility), "TryFindBestBetterStoreCellFor");
-            var modifiedMethod = AccessTools.Method(typeof(Haul_Patch), "TryFindBestBetterStoreCellForReplacement");
-            return original.MethodReplacer(originalMethod, modifiedMethod);
+            //Skip the TryMakePreToilReservations method
+            return false;
         }
 
-
-        static bool TryFindBestBetterStoreCellForReplacement(Thing t, Pawn carrier, Map map, StoragePriority currentPriority, Faction faction, out IntVec3 foundCell, bool needAccurateResult = true)
+        static void Postfix(Verse.AI.JobDriver_HaulToCell __instance, ref bool __result, bool errorOnFailed)
         {
-            //HaulToStack.Instance.Logger.Trace("FINDING STORAGE FOR ITEM " + t.def.defName);
+            Pawn pawn = __instance.pawn;
+            Job job = __instance.job;
+            const TargetIndex HaulableInd = TargetIndex.A;
+            const TargetIndex StoreCellInd = TargetIndex.B;
+            LocalTargetInfo thing = __instance.job.GetTarget(HaulableInd);
+            LocalTargetInfo destination = __instance.job.GetTarget(StoreCellInd);
 
-            List<SlotGroup> allGroupsListInPriorityOrder = map.slotGroupManager.AllGroupsListInPriorityOrder;
-            if (allGroupsListInPriorityOrder.Count == 0)
+            //HaulToStack.Instance.Logger.Trace("FINDING STORAGE FOR ITEM " + job.targetA.Thing.def.defName);
+            //HaulToStack.Instance.Logger.Trace("Error on fail: " + errorOnFailed.ToString());
+            //HaulToStack.Instance.Logger.Trace("Initial result: " + __result.ToString());
+
+            //If stacklimit for this item is 1, then reserve the tile and the thing
+            if (job.targetA.Thing.def.stackLimit <= 1)
             {
-                foundCell = IntVec3.Invalid;
-                return false;
-            }
-            IntVec3 a = (!t.SpawnedOrAnyParentSpawned) ? carrier.PositionHeld : t.PositionHeld;
-            StoragePriority storagePriority = currentPriority;
-            float num = 2.14748365E+09f;
-            IntVec3 intVec = default(IntVec3);
-            IntVec3 plannedTile = default(IntVec3);
-            bool flag = false;
-            int count = allGroupsListInPriorityOrder.Count;
-            for (int i = 0; i < count; i++)
-            {
-                SlotGroup slotGroup = allGroupsListInPriorityOrder[i];
-                StoragePriority priority = slotGroup.Settings.Priority;
-                if (priority < storagePriority || priority <= currentPriority)
+                HaulToStack.Instance.Logger.Trace("Inside full reservation mode");
+                if (pawn.Reserve(destination, job, 1, -1, null, errorOnFailed))
                 {
-                    break;
+                    __result = pawn.Reserve(thing, job, 1, -1, null, errorOnFailed);
+                    HaulToStack.Instance.Logger.Trace("Result: " + __result.ToString());
                 }
-                if (slotGroup.Settings.AllowedToAccept(t))
+                else
                 {
-                    List<IntVec3> cellsList = slotGroup.CellsList;
-                    //count2 is the size of the cell
-                    int count2 = cellsList.Count;
-                    int num2;
-                    if (needAccurateResult)
-                    {
-                        num2 = Mathf.FloorToInt((float)count2 * Rand.Range(0.005f, 0.018f));
-                        //Log.Message("num2: " + num2);
-                    }
-                    else
-                    {
-                        num2 = 0;
-                    }
-                    for (int j = 0; j < count2; j++)
-                    {
-                        IntVec3 intVec2 = cellsList[j];
-                        float num3 = (float)(a - intVec2).LengthHorizontalSquared;
-
-                        if (!(num3 <= num))
-                        {
-#if DEBUG
-                            HaulToStack.Instance.Logger.Trace("Game usually doesn't attempt to haul on this condition");
-#endif
-                            if (CellIsReachable(intVec2, map, t, carrier, faction))
-                            {
-                                string stackSituation = CellCanStack(intVec2, map, t);
-                                if (stackSituation.Equals("stackable"))
-                                {
-                                    //This is the ideal situation
-#if DEBUG
-                                    HaulToStack.Instance.Logger.Trace("We found a stacker!");
-#endif
-                                    flag = true;
-                                    intVec = intVec2;
-                                    num = num3;
-                                    storagePriority = priority;
-                                    if (j >= num2)
-                                    {
-                                        break;
-                                    }
-                                }
-                            }
-                        }
-                        else
-                        {
-                            if (CellIsReachable(intVec2, map, t, carrier, faction))
-                            {
-                                string stackSituation = CellCanStack(intVec2, map, t);
-
-                                if (stackSituation.Equals("clear"))
-                                {
-                                    flag = true;
-                                    intVec = intVec2;
-                                    plannedTile = intVec;
-                                    num = num3;
-                                    storagePriority = priority;
-                                }
-                                else if (stackSituation.Equals("stackable"))
-                                {
-                                    //This is the ideal situation
-#if DEBUG
-                                    HaulToStack.Instance.Logger.Trace("We found a stacker!");
-#endif
-                                    flag = true;
-                                    intVec = intVec2;
-                                    num = num3;
-                                    storagePriority = priority;
-                                    if (j >= num2)
-                                    {
-                                        break;
-                                    }
-                                }
-                            }
-                        }
-                        //if (IsGoodStoreCell_Replacement(intVec2, map, t, carrier, faction))
-                        //{
-                        //    flag = true;
-                        //    intVec = intVec2;
-                        //    num = num3;
-                        //    storagePriority = priority;
-                        //    if (j >= num2)
-                        //    {
-                        //        break;
-                        //    }
-                        //}
-
-                    }
+                    HaulToStack.Instance.Logger.Trace("Failed to reserve, setting result to false");
+                    __result = false;
                 }
             }
-
-            if (!flag)
+            //We always want to reserve what we're hauling
+            //Kluch: In the future we might want to set this based on whether or not the pawn can haul the whole stack or not
+            else
             {
-                foundCell = IntVec3.Invalid;
-                //HaulToStack.Instance.Logger.Trace("Failed to find a stack location");
-                return false;
+                HaulToStack.Instance.Logger.Trace("Reserving just the thing");
+                __result = pawn.Reserve(thing, job, 1, -1, null, errorOnFailed);
+                HaulToStack.Instance.Logger.Trace("Result: " + __result.ToString());
             }
-            foundCell = intVec;
-
-            /**
-             * TODO: continue working on this
-             */
-            //We're starting a new stack
-            if (plannedTile.Equals(foundCell))
-            {
-
-#if DEBUG
-                PlannedHauls ph = new PlannedHauls(plannedTile, t, t.stackCount);
-                HaulToStack.Instance.Logger.Trace("Adding to Planned Hauls");
-                HaulToStack.Instance.Logger.Trace(t.def.defName + " " + t.stackCount);
-                plannedHaulList.Add(ph);
-#endif
-
-            }
-
-            //HaulToStack.Instance.Logger.Trace("We found a stack location");
-            return true;
         }
 
-        static bool CellIsReachable(IntVec3 c, Map map, Thing t, Pawn carrier, Faction faction)
-        {
-            if (carrier != null && c.IsForbidden(carrier))
-            {
-                return false;
-            }
-            /**
-             * Commented out code here stops a check for tile reservation.
-             * It might be a good idea to turn this back on now that the job doesn't reserve the tile
-             */
-            if (carrier != null)
-            {
-                if (!carrier.CanReserveNew(c))
-                {
-                    return false;
-                }
-            }
-            else if (faction != null && map.reservationManager.IsReservedByAnyoneOf(c, faction))
-            {
-                return false;
-            }
-            return !c.ContainsStaticFire(map) && (carrier == null || carrier.Map.reachability.CanReach((!t.SpawnedOrAnyParentSpawned) ? carrier.PositionHeld : t.PositionHeld, c, PathEndMode.ClosestTouch, TraverseParms.For(carrier, Danger.Deadly, TraverseMode.ByPawn, false)));
-        }
-
-
-        static string CellCanStack(IntVec3 c, Map map, Thing thing)
-        {
-            List<Thing> list = map.thingGrid.ThingsListAt(c);
-            bool potentialStack = false;
-            for (int i = 0; i < list.Count; i++)
-            {
-                Thing thing2 = list[i];
-
-                //HaulToStack.Instance.Logger.Trace("Item on tile is: " + thing2.def.defName);
-                //HaulToStack.Instance.Logger.Trace("Item on hand is: " + thing.def.defName);
-
-                if (thing2.def.EverStoreable)
-                {
-                    if (!thing2.CanStackWith(thing))
-                    {
-                        //HaulToStack.Instance.Logger.Trace("Can't stack on eachother");
-                        return "unusable";
-                    }
-                    if (thing2.stackCount >= thing.def.stackLimit)
-                    {
-                        //HaulToStack.Instance.Logger.Trace("Stack count issue");
-                        return "unusable";
-                    }
-                }
-                if (thing2.def.entityDefToBuild != null && thing2.def.entityDefToBuild.passability != Traversability.Standable)
-                {
-                    //HaulToStack.Instance.Logger.Trace("impassible terrain");
-                    return "unusable";
-                }
-                if (thing2.def.surfaceType == SurfaceType.None && thing2.def.passability != Traversability.Standable)
-                {
-                    //HaulToStack.Instance.Logger.Trace("different impassible terrain");
-                    return "unusable";
-                }
-                if (thing2.def.defName.Equals(thing.def.defName))
-                {
-                    potentialStack = true;
-                }
-            }
-            if(potentialStack)
-            {
-                return "stackable";
-            }
-            return "clear";
-        }
-
-
-        //static bool IsGoodStoreCell_Replacement(IntVec3 c, Map map, Thing t, Pawn carrier, Faction faction)
-        //{
-        //    if (carrier != null && c.IsForbidden(carrier))
-        //    {
-        //        HaulToStack.Instance.Logger.Trace("IsGoodStoreCell: " + 1);
-        //        return false;
-        //    }
-        //    if (!NoStorageBlockersIn(c, map, t))
-        //    {
-        //        HaulToStack.Instance.Logger.Trace("IsGoodStoreCell: " + 2);
-        //        return false;
-        //    }
-        //    if (carrier != null)
-        //    {
-        //        if (!carrier.CanReserve(c, 1, -1, null, false))
-        //        {
-        //            HaulToStack.Instance.Logger.Trace("IsGoodStoreCell: " + 3);
-        //            return false;
-        //        }
-        //    }
-        //    else if (map.reservationManager.IsReserved(c, faction))
-        //    {
-        //        HaulToStack.Instance.Logger.Trace("IsGoodStoreCell: " + 4);
-        //        return false;
-        //    }
-        //    HaulToStack.Instance.Logger.Trace("IsGoodStoreCell: " + 5);
-        //    return !c.ContainsStaticFire(map) && (carrier == null || carrier.Map.reachability.CanReach((!t.SpawnedOrAnyParentSpawned) ? carrier.PositionHeld : t.PositionHeld, c, PathEndMode.ClosestTouch, TraverseParms.For(carrier, Danger.Deadly, TraverseMode.ByPawn, false)));
-        //}
-
-
-        //private static bool NoStorageBlockersIn(IntVec3 c, Map map, Thing thing)
-        //{
-        //    List<Thing> list = map.thingGrid.ThingsListAt(c);
-        //    for (int i = 0; i < list.Count; i++)
-        //    {
-        //        Thing thing2 = list[i];
-        //        if (thing2.def.EverStoreable)
-        //        {
-        //            if (!thing2.CanStackWith(thing))
-        //            {
-        //                HaulToStack.Instance.Logger.Trace("NoStorageBlockersIn: " + 1);
-        //                return false;
-        //            }
-        //            if (thing2.stackCount >= thing.def.stackLimit)
-        //            {
-        //                HaulToStack.Instance.Logger.Trace("NoStorageBlockersIn: " + 2);
-        //                return false;
-        //            }
-        //        }
-        //        if (thing2.def.entityDefToBuild != null && thing2.def.entityDefToBuild.passability != Traversability.Standable)
-        //        {
-        //            HaulToStack.Instance.Logger.Trace("NoStorageBlockersIn: " + 3);
-        //            return false;
-        //        }
-        //        if (thing2.def.surfaceType == SurfaceType.None && thing2.def.passability != Traversability.Standable)
-        //        {
-        //            HaulToStack.Instance.Logger.Trace("NoStorageBlockersIn: " + 4);
-        //            return false;
-        //        }
-        //    }
-        //    HaulToStack.Instance.Logger.Trace("NoStorageBlockersIn: " + 5);
-        //    return true;
-        //}
     }
 
 }
--- Source/JobDriver_HaulToStack.cs
+++ Source/JobDriver_HaulToStack.cs
@@ -12,7 +12,6 @@ namespace HaulToStack
 {
     class JobDriver_HaulToStack : JobDriver
     {
-
         //Working vars
         private bool forbiddenInitially;
 
@@ -29,36 +28,36 @@ namespace HaulToStack
 
         public override string GetReport()
         {
-            IntVec3 destLoc = job.targetB.Cell;
+            var destLoc = job.targetB.Cell;
 
             Thing hauledThing = null;
             if (pawn.CurJob == job && pawn.carryTracker.CarriedThing != null)
                 hauledThing = pawn.carryTracker.CarriedThing;
-            else
+            else if (TargetThingA != null && TargetThingA.Spawned)
                 hauledThing = TargetThingA;
 
-            if( hauledThing == null )
+            if (hauledThing == null)
                 return "ReportHaulingUnknown".Translate();
 
             string destName = null;
             var destGroup = destLoc.GetSlotGroup(Map);
-            if ( destGroup != null )
+            if (destGroup != null)
                 destName = destGroup.parent.SlotYielderLabel();
 
             if (destName != null)
-                return "ReportHaulingTo".Translate(hauledThing.LabelCap, destName);
+                return "ReportHaulingTo".Translate(hauledThing.Label, destName);
             else
-                return "ReportHauling".Translate(hauledThing.LabelCap);
+                return "ReportHauling".Translate(hauledThing.Label);
         }
 
-        public override bool TryMakePreToilReservations()
+        public override bool TryMakePreToilReservations(bool errorOnFailed)
         {
             //if stacklimit for this item is 1, then reserve the tile
-            if ( job.targetA.Thing.def.stackLimit <=  1)
-                return pawn.Reserve(job.GetTarget(StoreCellInd), job)
-                    && pawn.Reserve(job.GetTarget(HaulableInd), job);
+            if (job.targetA.Thing.def.stackLimit <= 1)
+                return pawn.Reserve(job.GetTarget(StoreCellInd), job, errorOnFailed: errorOnFailed)
+                && pawn.Reserve(job.GetTarget(HaulableInd), job, errorOnFailed: errorOnFailed);
             else
-                return pawn.Reserve(job.GetTarget(HaulableInd), job);
+                return pawn.Reserve(job.GetTarget(HaulableInd), job, errorOnFailed: errorOnFailed);
         }
 
         //Kluch: I might need to modify this code, not sure yet
@@ -75,15 +74,15 @@ namespace HaulToStack
         protected override IEnumerable<Toil> MakeNewToils()
         {
             //Set fail conditions
-            this.FailOnDestroyedOrNull( HaulableInd );
-            this.FailOnBurningImmobile( StoreCellInd );
+            this.FailOnDestroyedOrNull(HaulableInd);
+            this.FailOnBurningImmobile(StoreCellInd);
 
             //Note we only fail on forbidden if the target doesn't start that way
             //This helps haul-aside jobs on forbidden items
             //
             // TODO instead of this, just use Job.ignoreForbidden where appropriate
             //
-            if ( !forbiddenInitially )
+            if (!forbiddenInitially)
                 this.FailOnForbidden(HaulableInd);
 
             //Reserve thing to be stored
@@ -93,62 +92,62 @@ namespace HaulToStack
 #if DEBUG
             HaulToStack.Instance.Logger.Trace("Current Job: " + pawn.CurJob.def.defName);
 #endif
-            
+
             //Reserve the location to store
-            //Only do this if (current stack size + what pawn is currently holding + targetA stack size) < things max stack size
-            if (HaulUtils.ShouldReserveHaulLocation(job.targetA.Thing, job.targetB.Cell, pawn, Map))
-            {
+            //Only do this if (current stack size + what pawn is currently holding + targetA stack size) >= things max stack size
+            //if (HaulUtils.ShouldReserveHaulLocation(job.targetA.Thing, job.targetB.Cell, pawn, Map))
+            //{
                 var reserveTargetB = Toils_Reserve.Reserve(StoreCellInd);
                 yield return reserveTargetB;
-            }
+            //}
 
 
             Toil toilGoto = null;
-            toilGoto = Toils_Goto.GotoThing( HaulableInd, PathEndMode.ClosestTouch )
+            toilGoto = Toils_Goto.GotoThing(HaulableInd, PathEndMode.ClosestTouch)
                 .FailOnSomeonePhysicallyInteracting(HaulableInd)
-                .FailOn( () =>
-                {
-                //Note we don't fail on losing hauling designation
-                //Because that's a special case anyway
+                .FailOn(() =>
+               {
+                    //Note we don't fail on losing hauling designation
+                    //Because that's a special case anyway
 
-                //While hauling to cell storage, ensure storage dest is still valid
-                Pawn actor = toilGoto.actor;
-                    Job curJob = actor.jobs.curJob;
-                    if ( curJob.haulMode == HaulMode.ToCellStorage )
-                    {
-                        Thing haulThing = curJob.GetTarget( HaulableInd ).Thing;
+                    //While hauling to cell storage, ensure storage dest is still valid
+                    Pawn actor = toilGoto.actor;
+                   Job curJob = actor.jobs.curJob;
+                   if (curJob.haulMode == HaulMode.ToCellStorage)
+                   {
+                       Thing haulThing = curJob.GetTarget(HaulableInd).Thing;
 
-                        IntVec3 destLoc = actor.jobs.curJob.GetTarget(TargetIndex.B).Cell;
-                        if (!destLoc.IsValidStorageFor(Map, haulThing))
-                            return true;
-                    }
+                       IntVec3 destLoc = actor.jobs.curJob.GetTarget(TargetIndex.B).Cell;
+                       if (!destLoc.IsValidStorageFor(Map, haulThing))
+                           return true;
+                   }
 
-                    return false;
-                });
+                   return false;
+               });
             yield return toilGoto;
 
-            yield return Toils_Haul.StartCarryThing( HaulableInd, subtractNumTakenFromJobCount: true );
+            yield return Toils_Haul.StartCarryThing(HaulableInd, subtractNumTakenFromJobCount: true);
 
-            if ( job.haulOpportunisticDuplicates )
+            if (job.haulOpportunisticDuplicates)
             {
-                //yield return Toils_Haul.CheckForGetOpportunityDuplicate(reserveTargetA, HaulableInd, StoreCellInd);
-                yield return HaulUtils.CheckForGetOpportunityDuplicateReplace(reserveTargetA, HaulableInd, StoreCellInd);
+                yield return Toils_Haul.CheckForGetOpportunityDuplicate(reserveTargetA, HaulableInd, StoreCellInd);
+                //yield return HaulUtils.CheckForGetOpportunityDuplicateReplace(reserveTargetA, HaulableInd, StoreCellInd);
 
-//#if DEBUG
-//                HaulToStack.Instance.Logger.Trace("Opportunistic pickup");
-//                if (getDups == null)
-//                    HaulToStack.Instance.Logger.Trace("There are no opportunistic dups");
-//#endif
-//                if (HaulUtils.ShouldReserveHaulLocation(nextJob.job.targetA.Thing, job.targetB.Cell, pawn, Map))
-//                {
-//                    var reserveTargetB = Toils_Reserve.Reserve(StoreCellInd);
-//                    yield return reserveTargetB;
-//                }
-                
+                //#if DEBUG
+                //                HaulToStack.Instance.Logger.Trace("Opportunistic pickup");
+                //                if (getDups == null)
+                //                    HaulToStack.Instance.Logger.Trace("There are no opportunistic dups");
+                //#endif
+                //                if (HaulUtils.ShouldReserveHaulLocation(nextJob.job.targetA.Thing, job.targetB.Cell, pawn, Map))
+                //                {
+                //                    var reserveTargetB = Toils_Reserve.Reserve(StoreCellInd);
+                //                    yield return reserveTargetB;
+                //                }
+
             }
-                
 
-            Toil carryToCell = Toils_Haul.CarryHauledThingToCell( StoreCellInd );
+
+            Toil carryToCell = Toils_Haul.CarryHauledThingToCell(StoreCellInd);
             yield return carryToCell;
 
             yield return Toils_Haul.PlaceHauledThingInCell(StoreCellInd, carryToCell, true);
